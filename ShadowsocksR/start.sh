6646ddbfb0dfef43a9461fa1f17849886e67ea95
k <k@kdaye.com>
2017-05-27 20:17:19 +0800

    add port

--- ShadowsocksR/start.sh
+++ ShadowsocksR/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-nohup ss-tunnel -c /etc/config.json -L "8.8.8.8:53" -u &
+nohup ss-tunnel -c /etc/config.json -l 53 -L "8.8.8.8:53" -u &
 sleep 5
-nohup ss-redir -c /etc/config.json  &
+nohup ss-redir -c /etc/config.json  -l 1081 &
 sleep 5
 nohup ss-local -c /etc/config.json -u &
 sleep 5

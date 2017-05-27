37d6ecf25d8087b2fe6c98ba8bd7976764358fb3
k <k@kdaye.com>
2017-05-27 20:23:53 +0800

    fixed bind

--- ShadowsocksR/start.sh
+++ ShadowsocksR/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-nohup ss-tunnel -c /etc/config.json -l 53 -L "8.8.8.8:53" -u &
+nohup ss-tunnel -c /etc/config.json -b 0.0.0.0 -l 53 -L "8.8.8.8:53" -u &
 sleep 5
-nohup ss-redir -c /etc/config.json  -l 1081 &
+nohup ss-redir -c /etc/config.json  -b 0.0.0.0 -l 1081 &
 sleep 5
-nohup ss-local -c /etc/config.json -u &
+nohup ss-local -c /etc/config.json -b 0.0.0.0 -l 1080 -u &
 sleep 5
 ss-server -c /etc/config.json -u

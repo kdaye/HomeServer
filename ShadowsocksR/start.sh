cf873ecfd4aa0fdeb98e9cd195813127e6552e65
k <k@kdaye.com>
2017-05-27 20:28:53 +0800

    change the address

--- ShadowsocksR/start.sh
+++ ShadowsocksR/start.sh
@@ -5,4 +5,4 @@ nohup ss-redir -c /etc/config.json  -b 0.0.0.0 -l 1081 &
 sleep 5
 nohup ss-local -c /etc/config.json -b 0.0.0.0 -l 1080 -u &
 sleep 5
-ss-server -c /etc/config.json -u
+ss-server -c /etc/config.json -s 0.0.0.0 -u

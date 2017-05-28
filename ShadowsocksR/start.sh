03047b0331127715937ab58ac4bb29e93302ddd2
k <k@kdaye.com>
2017-05-28 13:32:41 +0800

    update

--- ShadowsocksR/start.sh
+++ ShadowsocksR/start.sh
@@ -3,6 +3,6 @@ nohup ss-tunnel -c /etc/config.json -b 0.0.0.0 -l 53 -L "8.8.8.8:53" -u &
 sleep 5
 nohup ss-redir -c /etc/config.json  -b 0.0.0.0 -l 1081 &
 sleep 5
-nohup ss-local -c /etc/config.json -b 0.0.0.0 -l 1080 -u &
+nohup ss-local -c /etc/config.json -b 0.0.0.0 -l 1080 -u --fast-open &
 sleep 5
-ss-server -c /etc/config.json -s 0.0.0.0 -u
+ss-server -c /etc/config.json -s 0.0.0.0 -u --fast-open

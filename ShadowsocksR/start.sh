9c0ac65aa22255b89167b8bde8d73ee807483757
k <k@kdaye.com>
2017-05-28 14:28:13 +0800

    changed

--- ShadowsocksR/start.sh
+++ ShadowsocksR/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-nohup ss-tunnel -c /etc/config.json -b 0.0.0.0 -l 53 -L "8.8.8.8:53" -u &
-sleep 5
 nohup ss-redir -c /etc/config.json  -b 0.0.0.0 -l 1081 &
 sleep 5
-nohup ss-local -c /etc/config.json -b 0.0.0.0 -l 1080 -u --fast-open &
-sleep 5
-ss-server -c /etc/config.json -s 0.0.0.0 -u --fast-open
+ss-local -c /etc/config.json -b 0.0.0.0 -l 1080 -u --fast-open
